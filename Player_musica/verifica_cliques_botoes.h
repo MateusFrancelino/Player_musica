41dbdca45541f5021d76f29cac5d7d0e931f77ac
kessystaub <51220952+kessystaub@users.noreply.github.com>
2019-11-28 12:04:28 -0300

    Update verifica_cliques_botoes.h

--- Player_musica/verifica_cliques_botoes.h
+++ Player_musica/verifica_cliques_botoes.h
@@ -4,7 +4,13 @@
 #include <iostream>
 #include "declara_botao.h"
 #include <SDL2/SDL_mixer.h>
+
+
+#include <cstdlib>
+#include <ctime>
+
 using namespace std;
+
 
 static Mix_Music *musica;
 static bool tocando = true;
@@ -12,7 +18,16 @@ static bool acabou = false;
 static bool repeat = false;
 static bool random = false;
 
-int Verifica(botoes* Botao,int x,int y,string* local,int aux,SDL_Renderer* render,int Tamanho){
+
+int Random(int Tamanho){
+    srand((unsigned)time(0));
+    int x;
+    x=(rand()%Tamanho);
+    cout<<x<<endl;
+    return x;
+}
+
+int Verifica(botoes* Botao,int x,int y,string* local,int aux,SDL_Renderer* render, int Tamanho){
     cout <<"X:" <<x<<" Y:" << y << endl;
     if(x>=Botao->Bskip.destino.x&&x<=Botao->Bskip.destino.x+Botao->Bskip.destino.w&&y>=Botao->Bskip.destino.y&&y<=Botao->Bskip.destino.y+Botao->Bskip.destino.h){
             aux++;
@@ -32,7 +47,7 @@ int Verifica(botoes* Botao,int x,int y,string* local,int aux,SDL_Renderer* rende
     }
     else if (x>=Botao->Bpause.destino.x&&x<=Botao->Bpause.destino.x+Botao->Bpause.destino.w&&y>=Botao->Bpause.destino.y&&y<=Botao->Bpause.destino.y+Botao->Bpause.destino.h){
 
-        if(!tocando){
+        if(tocando==false){
             Mix_ResumeMusic();
             tocando=true;
 
@@ -65,7 +80,6 @@ int Verifica(botoes* Botao,int x,int y,string* local,int aux,SDL_Renderer* rende
     }
     else if(x>=Botao->Brepeat.destino.x&&x<=Botao->Brepeat.destino.x+Botao->Brepeat.destino.w&&y>=Botao->Brepeat.destino.y&&y<=Botao->Brepeat.destino.y+Botao->Brepeat.destino.h){
 
-
         if(repeat)
         {
             Botao->Brepeat.destino.x=290;
@@ -91,17 +105,30 @@ int Verifica(botoes* Botao,int x,int y,string* local,int aux,SDL_Renderer* rende
         return aux;
 }
     else if(x>=Botao->Brandom.destino.x&&x<=Botao->Brandom.destino.x+Botao->Brandom.destino.w&&y>=Botao->Brandom.destino.y&&y<=Botao->Brandom.destino.y+Botao->Brandom.destino.h){
-            Botao->Brandom.destino.x=480;
-            Botao->Brandom.destino.y=480;
-            Botao->Brandom.destino.h=40;
-            Botao->Brandom.destino.w=40;
-            Botao->Brandom.textura=CarregaImagem("random_clique.bmp",render);
-            if (random){
 
+         if(random){
+                Botao->Brandom.destino.x=480;
+                Botao->Brandom.destino.y=480;
+                Botao->Brandom.destino.h=40;
+                Botao->Brandom.destino.w=40;
+                Botao->Brandom.textura=CarregaImagem("random.bmp",render);
+                random=false;
             }
-            else{
+         else
+            {
+                Botao->Brandom.destino.x=480;
+                Botao->Brandom.destino.y=480;
+                Botao->Brandom.destino.h=40;
+                Botao->Brandom.destino.w=40;
+                Botao->Brandom.textura=CarregaImagem("random_clique.bmp",render);
 
+                aux=Random(Tamanho);
+             
+                musica= Mix_LoadMUS(local[aux].c_str());
+                Mix_PlayMusic(musica,0);
+                random=true;
             }
+
     }
 
 
