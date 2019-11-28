786b73ed7216a4549a285d33d0edfcaaba2afed5
kessystaub <51220952+kessystaub@users.noreply.github.com>
2019-11-28 03:50:08 -0300

    Create verifica_cliques_botoes.h

---
+++ Player_musica/verifica_cliques_botoes.h
@@ -0,0 +1,102 @@
+#ifndef VERIFICA_CLIQUES_BOTOES_H
+#define VERIFICA_CLIQUES_BOTOES_H
+#include <SDL2/SDL.h>
+#include <iostream>
+#include "declara_botao.h"
+#include <SDL2/SDL_mixer.h>
+using namespace std;
+
+Mix_Music *musica;
+bool tocando = true;
+bool acabou =false;
+int repeat=2;
+
+int Verifica(botoes* Botao,int x,int y,string* local,int aux,SDL_Renderer* render){
+    cout <<"X:" <<x<<" Y:" << y << endl;
+    if(x>=Botao->Bskip.destino.x&&x<=Botao->Bskip.destino.x+Botao->Bskip.destino.w&&y>=Botao->Bskip.destino.y&&y<=Botao->Bskip.destino.y+Botao->Bskip.destino.h){
+            aux++;
+            cout<<endl<<"aux:"<<aux;
+            if(aux>12){
+            aux=0;
+            }
+            //cout<<local[aux]<<endl;
+            Botao->Bskip.destino.x=580;
+            Botao->Bskip.destino.y=460;
+            Botao->Bskip.destino.h=40;
+            Botao->Bskip.destino.w=40;
+            Mix_FreeMusic(musica);
+            musica= Mix_LoadMUS(local[aux].c_str());
+            Mix_PlayMusic(musica,0);
+            return aux;
+    }
+    else if (x>=Botao->Bpause.destino.x&&x<=Botao->Bpause.destino.x+Botao->Bpause.destino.w&&y>=Botao->Bpause.destino.y&&y<=Botao->Bpause.destino.y+Botao->Bpause.destino.h){
+
+        if(tocando==false){
+            Mix_ResumeMusic();
+            tocando=true;
+
+        }
+
+        else{
+            Mix_PauseMusic();
+            tocando=false;
+
+        }
+
+
+    }
+    else if(x>=Botao->Breturn.destino.x&&x<=Botao->Breturn.destino.x+Botao->Breturn.destino.w&&y>=Botao->Breturn.destino.y&&y<=Botao->Breturn.destino.y+Botao->Breturn.destino.h){
+            aux--;
+            cout<<endl<<"aux:"<<aux;
+            if(aux<0){
+            aux=11;
+            }
+            //cout<<local[aux]<<endl;
+            Botao->Breturn.destino.x=180;
+            Botao->Breturn.destino.y=460;
+            Botao->Breturn.destino.h=40;
+            Botao->Breturn.destino.w=40;
+            Mix_FreeMusic(musica);
+            musica= Mix_LoadMUS(local[aux].c_str());
+            Mix_PlayMusic(musica,0);
+            return aux;
+
+    }
+    else if(x>=Botao->Brepeat.destino.x&&x<=Botao->Brepeat.destino.x+Botao->Brepeat.destino.w&&y>=Botao->Brepeat.destino.y&&y<=Botao->Brepeat.destino.y+Botao->Brepeat.destino.h){
+
+        repeat++;
+        if(repeat%2==0)
+        {
+            Botao->Brepeat.destino.x=290;
+            Botao->Brepeat.destino.y=520;
+            Botao->Brepeat.destino.h=40;
+            Botao->Brepeat.destino.w=40;
+            Botao->Brepeat.textura=CarregaImagem("repeat.bmp",render);
+            musica= Mix_LoadMUS(local[aux].c_str());
+            Mix_PlayMusic(musica,0);
+        }
+        else
+        {
+            Botao->Brepeat.destino.x=290;
+            Botao->Brepeat.destino.y=520;
+            Botao->Brepeat.destino.h=40;
+            Botao->Brepeat.destino.w=40;
+            Botao->Brepeat.textura=CarregaImagem("repeat_clique.bmp",render);
+            musica= Mix_LoadMUS(local[aux].c_str());
+            Mix_PlayMusic(musica,-1);
+        }
+        return aux;
+}
+    else if(x>=Botao->Brandom.destino.x&&x<=Botao->Brandom.destino.x+Botao->Brandom.destino.w&&y>=Botao->Brandom.destino.y&&y<=Botao->Brandom.destino.y+Botao->Brandom.destino.h){
+            Botao->Brandom.destino.x=480;
+            Botao->Brandom.destino.y=480;
+            Botao->Brandom.destino.h=40;
+            Botao->Brandom.destino.w=40;
+            Botao->Brandom.textura=CarregaImagem("random_clique.bmp",render);
+    }
+
+
+
+}
+
+#endif // VERIFICA_CLIQUES_BOTOES_H
